db07510521894656beafefe63c229c27f1c65b0d
Michael Barlow <michaelbarlow7@gmail.com>
2013-07-06 19:43:02 +1000

    Deleting all commented out code to clean it up a bit

--- src/Bombermaaan/CDisplay.cpp
+++ src/Bombermaaan/CDisplay.cpp
@@ -61,42 +61,21 @@ CDisplay::~CDisplay (void)
 bool CDisplay::Create (int Width, int Height, bool FullScreen)
 {
     // Check if we have a connection with the resources
-//#ifdef WIN32
-    //ASSERT (m_hModule != NULL);
-//#endif
     
     int Depth = (FullScreen ? 16 : 0);
 	
     // If no display mode has been set yet or the current display mode is not the right one
-//#ifdef WIN32
-    //if (!m_DirectDraw.IsModeSet (Width, Height, Depth, FullScreen))
-//#else
     if (!m_SDLVideo.IsModeSet (Width, Height, Depth, FullScreen))
-//#endif
     {
 		    // Destroy DirectDraw/SDLVideo interface and the sprite tables
         Destroy ();
                 
         // If DirectDraw/SDLVideo object creation failed
-//#ifdef WIN32
-        //if (!m_DirectDraw.Create (Width, Height, Depth, FullScreen))
-//#else
         if (!m_SDLVideo.Create (Width, Height, Depth, FullScreen))
-//#endif
         {
             // Get out
             return false;
         }
-
-//#ifdef WIN32
-        // Set the RGB color for transparent pixels in sprites
-        // If it failed
-        //if (!m_DirectDraw.SetTransparentColor (0, 255, 0))
-        //{
-            // Get out, it failed
-            //return false;
-        //}
-//#endif        
 
         // Load the sprite tables. If at least one sprite table could not be loaded
         if (
@@ -231,11 +210,7 @@ bool CDisplay::Create (int Width, int Height, bool FullScreen)
         m_ViewOriginX = (Width - VIEW_WIDTH) / 2;
         m_ViewOriginY = (Height - VIEW_HEIGHT) / 2;
 
-//#ifdef WIN32
-        //m_DirectDraw.SetOrigin (m_ViewOriginX, m_ViewOriginY);
-//#else
         m_SDLVideo.SetOrigin (m_ViewOriginX, m_ViewOriginY);
-//#endif
     }
 
     // Everything went right
@@ -268,11 +243,7 @@ bool CDisplay::Create (EDisplayMode DisplayMode)
 void CDisplay::Destroy (void)
 {
     // Destroy DirectDraw/SDLVideo interface and the sprite tables
-//#ifdef WIN32
-    //m_DirectDraw.Destroy ();
-//#else
     m_SDLVideo.Destroy ();
-//#endif
 }
 
 //******************************************************************************************************************************
@@ -286,15 +257,9 @@ bool CDisplay::IsDisplayModeAvailable (EDisplayMode DisplayMode)
     // According to the display mode to test
     switch (DisplayMode)
     {
-//#ifdef WIN32
-        //case DISPLAYMODE_FULL1    : return m_DirectDraw.IsModeAvailable (320, 240, 16);
-        //case DISPLAYMODE_FULL2    : return m_DirectDraw.IsModeAvailable (512, 384, 16);
-        //case DISPLAYMODE_FULL3    : return m_DirectDraw.IsModeAvailable (640, 480, 16);
-//#else
         case DISPLAYMODE_FULL1    : return m_SDLVideo.IsModeAvailable (320, 240, 16);
         case DISPLAYMODE_FULL2    : return m_SDLVideo.IsModeAvailable (512, 384, 16);
         case DISPLAYMODE_FULL3    : return m_SDLVideo.IsModeAvailable (640, 480, 16);
-//#endif
         case DISPLAYMODE_WINDOWED : return true;
         default                   : return false; // Should never happen
     }
@@ -311,56 +276,17 @@ bool CDisplay::LoadSprites (int SpriteTableWidth,
                             bool Transparent,
                             int BMP_ID)
 {
-//#ifdef WIN32
-    // Load the bitmap as a resource
-    //HBITMAP hBitmap = (HBITMAP) LoadImage (m_hModule, MAKEINTRESOURCE(BMP_ID), IMAGE_BITMAP, 0, 0, LR_CREATEDIBSECTION);
-
-    // If it failed
-    //if (hBitmap == NULL)
-    //{
-        // Log failure
-        //theLog.WriteLine ("Display         => !!! Could not load resource image (%d) and create handle to bitmap.", BMP_ID);
-        //theLog.LogLastError ();
-
-        // Get out
-        //return false;
-    //}
-//#endif
-    // Create the sprites by giving the sprite table information and the handle to the bitmap.
-    // If it fails
-//#ifdef WIN32
-    //if (!m_DirectDraw.LoadSprites (SpriteTableWidth,
-//#else
     if (!m_SDLVideo.LoadSprites (SpriteTableWidth,
-//#endif
                                    SpriteTableHeight, 
                                    SpriteWidth, 
                                    SpriteHeight, 
                                    Transparent,
-//#ifdef WIN32
-                                   //hBitmap))
-//#else
                                    BMP_ID))
-//#endif
     {
         // Get out, failure
         return false;
     }
 
-//#ifdef WIN32
-    // We no longer need the hBitmap so delete it
-    // If it fails
-    //if (DeleteObject (hBitmap) == 0)
-    //{
-        // Log failure
-        //theLog.WriteLine ("Display         => !!! Could not delete handle to bitmap.");
-        //theLog.LogLastError ();
-
-        // Get out, failure
-        //return false;
-    //}
-//#endif
-																	 
     // Everything went right
     return true;
 }
